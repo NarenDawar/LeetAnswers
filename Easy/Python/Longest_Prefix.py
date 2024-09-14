4a422642c2d3224b094d0472bdf0aca8e2c83541
narenDaw <narend@vt.edu>
2024-09-14 13:10:19 -0400

    Added more descriptions.

---
+++ Easy/Python/Longest_Prefix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str: #Defining the return type.
+        # Edge case: if the input list is empty, return an empty string.
+        if not strs:
+            return ""
+
+        # Start by assuming the first string is the common prefix.
+        prefix = strs[0]
+
+        # Compare the prefix with each string in the list.
+        for s in strs[1:]:
+            # While the prefix is not a prefix of the current string, reduce it until the condition is met.
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                # If the prefix becomes empty, return an empty string.
+                if not prefix:
+                    return ""
+
+        return prefix
+
+#This code basically takes the first word in the list and continue reducing it until the prefix matches for all other words in the list.
+#For example, if I had "flower", "flour", and "flow", the code would reduce flower -> flo because all words have the same prefix. 
+# The .startswith() is a built in python function that checks if a word starts with a given string of character or individual character.
--- Easy/Python/Roman_To_Integer.py
+++ Easy/Python/Roman_To_Integer.py
@@ -23,3 +23,9 @@ class Solution:
             prev_value = current_value
         
         return total
+
+#This code revolves around making a dictionary in python that assigns values to strings.
+#You simply reverse the string and traverse through while keeping track of the "previous value" variable.
+#If the current value is less than the previous value, then we know that we must subtract the current value from the integer total.
+
+#EX: IX, you start at X, and current value is now 10. Since current value isn't less than previous, since previous doesn't exist, our total is now 10. Now we iterate, and since I (1) is less than X(10), we subtract 1 from the total.
