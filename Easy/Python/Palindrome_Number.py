b986925d87eb6c535cfbf1e4a2896658bdeee693
narenDaw <narend@vt.edu>
2024-09-14 12:16:09 -0400

    Added.

--- Easy/Python/Palindrome_Number.py
+++ Easy/Python/Palindrome_Number.py
@@ -6,5 +6,5 @@ class Solution:
 
 #Python has a lot of useful features, and this is a simple example. The [:::] format represents [start:stop:step].
 #So, in this instance, because there is no start or stop provided, it will by default consider the entire sequence, and -1 means to step backwards.
-# Stepping backwards means it will start at the last index and go towards the first index.
+#Stepping backwards means it will start at the last index and go towards the first index.
         
