3f4be91d17ffbc7891446bb994f30e0ef52c911a
piyushkomali <komali.piyush@gmail.com>
2024-09-25 00:02:03 -0400

    changed Best Time to Buy and Sell Stock

--- Easy/Python/Best_Time_To_Buy&Sell_Stock.py
+++ Easy/Python/Best_Time_To_Buy&Sell_Stock.py
@@ -1,29 +1,32 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0 #Create a variable to track max profit.
+#objective: use the array given to find the best time to buy and sell stock(cannot sell before you buy), then return max profit
+    # prices = [7,1,5,3,6,4]; best time to buy is i = 1 and sell is i = 4, maxprofit = 6-1 = 5
 
-        for i in range(len(prices)): #Iterator 1
-            for j in range(i+1, len(prices)): #Iterator 2
-                if(prices[j] - prices[i] > max_profit): 
-                    max_profit = prices[j] - prices[i] #Change max_profit is larger value is found.
-        return max_profit
-
-#This problem is very similar to the majority element problem, following the idea of having two iterators and a variable for tracking.
-#In this problem. we create a "max_profit" variable and create two iterators, one which is our starting and the other which checks all other values.
-#We subtract the value at the first iterator index from the second iterator index since it's a buying & selling process, and the aim is to buy low and sell high.
-#So, we essentially try subtracting all possible combinations that make sense, we cannot sell then buy, and find the largest profit available.
-#We initialize the max_profit to zero in case the list is descending, because then there is no way for us to sell at a price higher than our buying price. The problem..
-#..prompts us to return 0 if there is no profit available, which is why we set it to zero.
+# min needs to be highest possible value to make sure that the first array value is set to min
+# iterate through the prices array and compare min to current price to make sure you have the lowest price possible set to min and in turn the max profit
+# in the same loop calculate the profit from the min price to the current price you are looking at and decide whether that is greater than the max profit
+#                                    if it is greater than maxprofit than set that to maxprofit
+# return maxprofit
 
 #Time complexity: O(n)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min = float('inf')
+        maxProfit = 0
+        for price in prices:
+            if(price < min):
+                min = price
+            profit = price - min
+            if(profit > maxProfit):
+                maxProfit = profit
+        return maxProfit
+
+
 
 #EX: [7,6,5,4]
 
-#Step 1: First iterator is at 7 and second iterator goes through all other values. Subtractions checked: 6-7, 5-7, 4-7. None are greater than 0, so max_profit is 0.
-#Step 2: First iterator is at 6 and second iterator goes through all other values. Subtractions checked: 5-6, 4-6. None are greater than 0, so max_profit is 0.
-#..As you may have noticed, the list is descending, so we'll never get a positive number, meaning there is no way for us to make profit.
+#Step 1: Because the min is set at 7 and every number after that is decreasing, profit will always be negative therefore maxProfit will always stay at 0
+#                   return maxProfit (= 0)
 
 #EX: [9,8,10]
-
-#Step 1: First iterator is at 9 and second iterator goes through all other values. Subtractions checked: 8-9, 10-9. 10-9 is greater than 0, so max_profit is now 1 (10-9).
-#Step 2: First iterator is at 8 and second iterator goes through all other values. Subtractions checked: 10-8. 10-8 is greater than 1, so max_profit is now 2 (10-8).
+#Step 1: After 2nd iteration min is set at 8 and last number is 10, when profit is calculated 10 - 8 = 2, so maxProfit = 2
+#                   return maxProfit (= 2)
